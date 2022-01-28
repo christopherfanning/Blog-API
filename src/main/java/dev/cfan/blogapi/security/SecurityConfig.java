e71acedeb145bd3769a9070c7bc617e813244e84
Christopher Fanning <chris@christopherfanning.com>
2022-01-28 17:00:27 -0600

    Make a place for readme assets.

--- src/main/java/dev/cfan/blogapi/security/SecurityConfig.java
+++ src/main/java/dev/cfan/blogapi/security/SecurityConfig.java
@@ -1,4 +1,33 @@
 package dev.cfan.blogapi.security;
 
-public class SecurityConfigurer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // only allowed urls without JWT
+        http.authorizeRequests().antMatchers(
+                        "/auth/users", "/auth/users/login",
+                        "/auth/users/register", "/api/helloworld").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
