d62fb7a35a98927ff3156af414b247b2741339f7
Christopher Fanning <chris@christopherfanning.com>
2022-01-31 11:42:52 -0600

    Add annotations to UserRepository

--- src/main/java/dev/cfan/blogapi/repository/UserRepository.java
+++ src/main/java/dev/cfan/blogapi/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package dev.cfan.blogapi.repository;
 
-public class UserRepository {
+import dev.cfan.blogapi.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
 }
