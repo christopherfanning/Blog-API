dd366f003bddce84af746f480e886719ec87aecc
Christopher Fanning <chris@christopherfanning.com>
2022-01-31 13:25:10 -0600

    Add Lombok annotations on User model

--- src/main/java/dev/cfan/blogapi/domain/User.java
+++ src/main/java/dev/cfan/blogapi/domain/User.java
@@ -1,21 +1,31 @@
 package dev.cfan.blogapi.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private Long id;
 
     @Column
+    @Getter @Setter
     private String name;
 
     @Column
+    @Getter @Setter
     private String email;
 
     @Column
+    @Setter
     private String password;
 
 
