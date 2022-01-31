08dcd4beceb4fe1bf8a5a9d9188ee31cb09d96a5
Christopher Fanning <chris@christopherfanning.com>
2022-01-31 11:30:36 -0600

    Add Post model fields

--- src/main/java/dev/cfan/blogapi/domain/Post.java
+++ src/main/java/dev/cfan/blogapi/domain/Post.java
@@ -1,4 +1,17 @@
 package dev.cfan.blogapi.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String postContent;
+    
 }
