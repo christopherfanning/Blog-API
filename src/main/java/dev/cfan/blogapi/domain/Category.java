7e5b3d6bc22158a2e59e4d52d7c51daee280f174
Christopher Fanning <chris@christopherfanning.com>
2022-01-31 11:33:35 -0600

    Add Category model fields

--- src/main/java/dev/cfan/blogapi/domain/Category.java
+++ src/main/java/dev/cfan/blogapi/domain/Category.java
@@ -1,4 +1,19 @@
 package dev.cfan.blogapi.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    
 }
--- src/main/java/dev/cfan/blogapi/domain/Post.java
+++ src/main/java/dev/cfan/blogapi/domain/Post.java
@@ -13,5 +13,5 @@ public class Post {
 
     @Column
     private String postContent;
-    
+
 }
