9aa55422593fad3bc9ed972f50347a9fd77bbc62
Christopher Fanning <chris@christopherfanning.com>
2022-01-31 11:31:13 -0600

    Add User model fields

--- src/main/java/dev/cfan/blogapi/domain/User.java
+++ src/main/java/dev/cfan/blogapi/domain/User.java
@@ -1,4 +1,26 @@
 package dev.cfan.blogapi.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+
+
+
+
+
 }
