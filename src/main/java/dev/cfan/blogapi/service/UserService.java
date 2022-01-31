f8a6ee6ff25d203f34ea27158359b28384324200
Christopher Fanning <chris@christopherfanning.com>
2022-01-31 11:40:31 -0600

    Add UserService

---
+++ src/main/java/dev/cfan/blogapi/service/UserService.java
@@ -0,0 +1,19 @@
+package dev.cfan.blogapi.service;
+
+import dev.cfan.blogapi.domain.User;
+import dev.cfan.blogapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        userRepository
+    }
+}
