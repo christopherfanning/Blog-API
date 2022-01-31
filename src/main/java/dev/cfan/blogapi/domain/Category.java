df456ddc42c300c5dd55af1f5316806ea9607717
Christopher Fanning <chris@christopherfanning.com>
2022-01-31 13:27:01 -0600

    Add Lombok to Post & Category models

--- src/main/java/dev/cfan/blogapi/domain/Category.java
+++ src/main/java/dev/cfan/blogapi/domain/Category.java
@@ -1,8 +1,14 @@
 package dev.cfan.blogapi.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@Getter @Setter
 public class Category {
 
     @Id
