b53be8cb4c62514ffd4cb1fd9a95a1364f80c0f3
Christopher Fanning <chris@christopherfanning.com>
2022-01-31 11:45:57 -0600

    Fix CategoryRepository

--- src/main/java/dev/cfan/blogapi/repository/CategoryRepository.java
+++ src/main/java/dev/cfan/blogapi/repository/CategoryRepository.java
@@ -1,4 +1,10 @@
 package dev.cfan.blogapi.repository;
 
-public class CategoryRepository {
+import dev.cfan.blogapi.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    
 }
